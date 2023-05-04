196988c0b2333890910edf0568f7e1c27384e060
menice4 <menicce4@gmail.com>
2023-05-04 15:56:56 +0100

    Revert "Reset"
    This reverts commit 919332de0cbdf8eda01d73cfce2d758b41b09720.

--- Assets/Scripts/TImeSlow.cs
+++ Assets/Scripts/TImeSlow.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class TImeSlow : MonoBehaviour
 {
     public bool Slowed;
-    public Movment Player;
-   
+    Movment player;
+    public float slowAmount = 0.1f;
+    public float slowDurtion = .5f;
+    private IEnumerator coroutine;
+    public bool Move;
+    public bool unlock;
+    public float Timeleft = .5f;
+
+
 
 
     // Start is called before the first frame update
+     void Awake()
+    {
+        player = GameObject.Find("FP_player").GetComponent<Movment>();
 
+    }
     void Start()
     {
+        unlock = false;
+        Move = false;
         Slowed = false;
-       
-
+        Debug.Log("time"+Time.timeScale);
+        Debug.Log("Movement speed" + player.movementSpeed);
 
 
 
@@ -28,34 +41,72 @@ public class TImeSlow : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Q) && Slowed == false)
         {
                 Slowed = true;
-            Time.timeScale = 0.001f;
+                Move = true;
+
+            Time.timeScale = slowAmount;
             Time.fixedDeltaTime = 0.02F * Time.timeScale;
+        
             
-            Player.movementSpeed = 2500f;
-            
-   
-            
- 
-
-
            
+            Debug.Log("Movement speed" + player.movementSpeed);
+
+
+
+
+
+
+
+
         }
-        else if (Input.GetKeyDown(KeyCode.Q) && Slowed == true)
+        else if (Input.GetKeyDown(KeyCode.Q) && Slowed == true )
         {
                 Slowed = false;
+           
             Time.timeScale = 1f;
-             Time.fixedDeltaTime = 0.02F ;
-            Player.movementSpeed = 5f;
+            Time.fixedDeltaTime = 0.02f;
+           
 
 
 
 
+        }
+
+
+        if (Move == true)
+
+        {
+            Move = false;
+            print("waiting");
+            coroutine = unSlow(Timeleft);
+            StartCoroutine(coroutine);
+       
+            
+        }
+        if (unlock == true)
+        {
+            Time.timeScale += (1f / slowDurtion) * Time.unscaledDeltaTime;
+            Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+            if (Time.timeScale == 1f)
+            {
+                Slowed = false;
+                unlock = false;
+            }
+
+            Debug.Log("time" + Time.timeScale);
+            Debug.Log("movement speed" + player.movementSpeed);
         }
 
 
     }
-
+    
+    IEnumerator unSlow(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        unlock = true;
+    }
 
 
 
 }
+
+
